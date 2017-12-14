9f8322bc03f9abc6fb5c173ad96972561fbb31fb
Zeph <zeph33@gmail.com>
2017-12-15 00:28:36 +0100

    Add getObjectValueByPath

--- app/utils.js
+++ app/utils.js
@@ -1,5 +1,5 @@
 'use strict'
-export { isObject, isEmpty, isArray, isBoolean, hasProperty, toString, objSet, objGet, merge }
+export { getObjectValueByPath, isObject, isEmpty, isArray, isBoolean, hasProperty, toString, objSet, objGet, merge }
 
 function hasProperty(obj, prop) {
   if (obj == null) {
@@ -93,4 +93,21 @@ function merge(target) {
     assign(target, JSON.parse(JSON.stringify(arguments[s])))
   }
   return target
+}
+
+function getObjectValueByPath (obj, path) {
+  // credit: http://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key#comment55278413_6491621
+  if (!path || path.constructor !== String) return
+  path = path.replace(/\[(\w+)\]/g, '.$1') // convert indexes to properties
+  path = path.replace(/^\./, '') // strip a leading dot
+  const a = path.split('.')
+  for (var i = 0, n = a.length; i < n; ++i) {
+    var k = a[i]
+    if (obj instanceof Object && k in obj) {
+      obj = obj[k]
+    } else {
+      return
+    }
+  }
+  return obj
 }
