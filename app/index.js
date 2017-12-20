24b3b6164bd77b65101b4321e17e29162bb7e56c
Zeph <zeph33@gmail.com>
2017-12-20 22:31:43 +0100

    Export utils to Vue.$utils

--- app/index.js
+++ app/index.js
@@ -4,10 +4,11 @@ import createLocalState from './store/localstorage'
 import {store, mixins } from './store'
 import App from './App'
 import components from './components'
-//import {zHttp} from './utils'
+import * as utils from './utils'
 import zHttp from './api/remote'
 
 Vue.prototype.$http = window.zHttp = new zHttp()
+Vue.prototype.$utils = window.utils = utils
 
 // add components
 components.forEach(component => Vue.component(component.name, component))
